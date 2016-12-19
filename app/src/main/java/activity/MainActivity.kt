a16d60ffd4827ee677054ef86443b77ac09edfa6
Yash <ysh.kant@gmail.com>
2016-12-19 20:57:11 +0530

    First Commit

---
+++ app/src/main/java/activity/MainActivity.kt
@@ -0,0 +1,16 @@
+package activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.yash.material_design_app.R
+
+class MainActivity : AppCompatActivity() {
+
+    //Material Design Implementation
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/adapter/NavigationDrawerAdapter.java
@@ -0,0 +1,12 @@
+package adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by yash on 18/12/16.
+ */
+
+public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDrawerAdapter.MyViewHolder>
+{
+
+}
